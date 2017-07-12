13a7eeaa15ec10ccfefc1936e5971cd20652a757
Priyam Mitra <pmitra43@gmail.com>
2017-07-12 16:04:34 +0530

    Updated workflowGenerator.py to generate given number of anomalies with a fixed duration taken as input

--- workflowGenerator.py
+++ workflowGenerator.py
@@ -1,20 +1,29 @@
-from random import randint
+from random import sample
 fileNumber=input("give cluster name: ")
 fileName="./json-data-generator-1.2.2-SNAPSHOT/conf/cpuUsageWorkflow{}.json".format(fileNumber)
 f = open(fileName, 'w')
 
 def print_to_file(s):
     f.write(s + "\n")
+
 time = int(input("Enter time in minutes: "))
-anomalyMinute=randint(0,time-1)
+anomalyDuration = int(input("Enter condition for anomaly in seconds: "))
+anomalyCount = int(input("Enter number of anomalies to be produced: "))
 time=time*60
+listPos=0;
+
+anomalyStartArray=list(map((lambda x: (x*anomalyDuration, (x+1)*anomalyDuration)),sample(range(int(time/anomalyDuration)),anomalyCount)))
+anomalyStartArray.sort()
+print(anomalyStartArray)
 # combine print statements
 print_to_file("{\n\t\"eventFrequency\": 1000,\n\t\"varyEventFrequency\": false,\n\t\"repeatWorkflow\": true,")
 print_to_file("\t\"timeBetweenRepeat\": 0,\n\t\"varyRepeatFrequency\": false,\n\t\"steps\": [{")
 for i in range(time):
     print_to_file("\t\t\"config\": [{\n\t\t\t\"nodeID\": 1,\n\t\t\t\"timestamp\":\"now()\",")
-    if i>=(anomalyMinute*60) and i<((anomalyMinute+1)*60):
+    if i>=anomalyStartArray[listPos][0] and i < anomalyStartArray[listPos][1]:
         print_to_file("\t\t\t\"cpu\":\"double(81.0,100.0)\"")
+        if i == (anomalyStartArray[listPos][1])-1:
+            listPos =(listPos + 1)%len(anomalyStartArray)
     else:
         print_to_file("\t\t\t\"cpu\":\"double(1.0,80.0)\"")
     print_to_file("\t\t}]")
