f67dd3909362d0a753a208a8946abed50e958700
Priyam Mitra <pmitra43@gmail.com>
2017-07-13 16:16:54 +0530

    Creating WorkflowGenerator class

--- workflowGenerator.py
+++ workflowGenerator.py
@@ -1,54 +1,58 @@
 from random import sample
-f = ""
-def getInputs():
-    fileNumber = input("give cluster name: ")
-    timeInMins = int(input("Enter time in minutes: "))
-    anomalyDuration = int(input("Enter condition for anomaly in seconds: "))
-    anomalyCount = int(input("Enter number of anomalies to be produced: "))
-    return (fileNumber, timeInMins, anomalyDuration, anomalyCount)
+class WorkflowGenerator:
 
-def createFile(fileNumber):
-    fileName = "./json-data-generator-1.2.2-SNAPSHOT/conf/cpuUsageWorkflow{}.json".format(fileNumber)
-    global f
-    f = open(fileName, 'w')
+    def __init__(self, arg=[]):
+        self.arg=arg
 
-def println(s):
-    f.write(s + "\n")
+    def getInputs(self):
+        self.clusterName = input("give cluster name: ")
+        self.nodeNumber = input("give node number: ")
+        self.time = int(input("Enter time in seconds: "))
+        self.anomalyDuration = int(input("Enter condition for anomaly in seconds: "))
+        self.anomalyCount = int(input("Enter number of anomalies to be produced: "))
 
-def generateAnomalousTimeSeq(timeInMins, anomalyDuration,anomalyCount):
-    time=timeInMins*60
-    timeSeq = sample(range(int(time/anomalyDuration)),anomalyCount)
-    anomalyStartArray=list(map((lambda x: (x*anomalyDuration, (x+1)*anomalyDuration) if x%2==0 else (x*anomalyDuration, (x+1)*anomalyDuration-1)),timeSeq))
-    anomalyStartArray.sort()
-    print(anomalyStartArray)
-    return anomalyStartArray
+    def createFile(self):
+        fileName = "./json-data-generator-1.2.2-SNAPSHOT/conf/cpuUsageWorkflow_{}_{}.json".format(self.clusterName, self.nodeNumber)
+        self.f = open(fileName, 'w')
 
-def printStepsToFile(time, anomalyStartArray):
-    listPos=0
-    println("{\n\t\"eventFrequency\": 1000,\n\t\"varyEventFrequency\": false,\n\t\"repeatWorkflow\": true,")
-    println("\t\"timeBetweenRepeat\": 0,\n\t\"varyRepeatFrequency\": false,\n\t\"steps\": [{")
-    for i in range(time):
-        println("\t\t\"config\": [{\n\t\t\t\"nodeID\": 1,\n\t\t\t\"timestamp\":\"now()\",")
-        if i>=anomalyStartArray[listPos][0] and i < anomalyStartArray[listPos][1]:
-            println("\t\t\t\"cpu\":\"double(81.0,100.0)\"")
-            if i == (anomalyStartArray[listPos][1])-1:
-                listPos =(listPos + 1)%len(anomalyStartArray)
+    def println(self,s):
+        self.f.write(s + "\n")
+
+    def generateAnomalousTimeSeq(self):
+        timeSeq = sample(range(int(self.time/self.anomalyDuration)),self.anomalyCount)
+        self.anomalyStartArray=list(map((lambda x: (x*self.anomalyDuration, (x+1)*self.anomalyDuration) if x%2==0 else (x*self.anomalyDuration, (x+1)*self.anomalyDuration-1)),timeSeq))
+        self.anomalyStartArray.sort()
+        print(self.anomalyStartArray)
+
+    def printStepsToFile(self):
+        listPos=0
+        self.println("{\n\t\"eventFrequency\": 1000,\n\t\"varyEventFrequency\": false,\n\t\"repeatWorkflow\": true,")
+        self.println("\t\"timeBetweenRepeat\": 0,\n\t\"varyRepeatFrequency\": false,\n\t\"steps\": [{")
+        for i in range(self.time):
+            self.println("\t\t\"config\": [{{\n\t\t\t\"cluster\": \"{}\",\n\t\t\t\"nodeID\": \"{}\",\n\t\t\t\"timestamp\":\"now()\",".format(self.clusterName, self.nodeNumber))
+            if i>=self.anomalyStartArray[listPos][0] and i < self.anomalyStartArray[listPos][1]:
+                self.println("\t\t\t\"cpu\":\"double(81.0,100.0)\"")
+                if i == (self.anomalyStartArray[listPos][1])-1:
+                    listPos =(listPos + 1)%len(self.anomalyStartArray)
+            else:
+                self.println("\t\t\t\"cpu\":\"double(1.0,80.0)\"")
+            self.println("\t\t}]")
+            if i == self.time-1:
+                self.println("\t}]")
+            else:
+                self.println("\t},{")
+
+        self.println("}")
+
+    def main(self):
+        if len(self.arg)==0:
+            self.getInputs()
         else:
-            println("\t\t\t\"cpu\":\"double(1.0,80.0)\"")
-        println("\t\t}]")
-        if i == time-1:
-            println("\t}]")
-        else:
-            println("\t},{")
-
-    println("}")
-
-def execute():
-    (fileNumber, timeInMins, anomalyDuration, anomalyCount) = getInputs()
-    createFile(fileNumber)
-    anomalyStartArray = generateAnomalousTimeSeq(timeInMins, anomalyDuration, anomalyCount)
-    printStepsToFile(timeInMins * 60, anomalyStartArray)
-    f.close()
+            (self.clusterName, self.nodeNumber, self.time, self.anomalyDuration, self.anomalyCount)=tuple(self.arg)
+        self.createFile()
+        self.generateAnomalousTimeSeq()
+        self.printStepsToFile()
+        self.f.close()
 
 if __name__=="__main__":
-    execute()
+    WorkflowGenerator().main()
